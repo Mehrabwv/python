7da7ba260084e3c3068518245b326492c706f26b
Mehrab <mehrab.wv@gmail.com>
2021-12-22 12:31:08 +0800

    22 dec

---
+++ temp.py
@@ -0,0 +1,10 @@
+from tkinter import *      
+root = Tk()      
+canvas = Canvas(root, width = 300, height = 300)      
+canvas.pack()      
+
+img = PhotoImage(file='gif-file.gif')      
+canvas.create_image(20,20, anchor=NW, image=img)      
+mainloop()
+
+# awowwowowwlowowowowiowiwiwiwiui
