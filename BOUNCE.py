6621078c10e0ff1548b7aee7dfdb6e9d6e5629de
Mehrab <mehrab.wv@gmail.com>
2021-12-31 13:14:58 +0800

    31

--- BOUNCE.py
+++ BOUNCE.py
@@ -3,8 +3,9 @@ import random
 import time
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         starts = [-3, -2, -1, 1, 2, 3]  
@@ -21,9 +22,11 @@ class Ball:
         self.canvas.move(self.id, self.x, self.y)      
         pos = self.canvas.coords(self.id)              
         if pos[1] <= 0:                                
-            self.y = 1
+            self.y = 3
         if pos[3] >= self.canvas_height:               
-            self.y = -1
+            self.y = -3
+        if self.hit_paddle(pos) == True:           
+            self.y = -3
         if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
@@ -43,13 +46,38 @@ class Paddle:
         
         
     def draw(self):
-        pass
-            
-           
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
         
         
-                 
+        
+        
+        
+    def turn_left(self, evt):
+        self.x = -2
+        
+        
+        
+        
+    def turn_right(self, evt):
+        self.x = 2
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
 
+    
 tk = Tk()
 tk.title("Bounce Game")
 tk.resizable(0, 0)
